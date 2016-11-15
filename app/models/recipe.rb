e62cb49665e09913563fe3de14149bca1357400d
vladich <vladichnaprimer@gmail.com>
2016-11-15 22:59:52 +0200

    Add devise gem, user model. Add authentication

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,18 +1,20 @@
 class Recipe < ApplicationRecord
 
-  has_many :ingredients
-  has_many :directions
+  belongs_to :user
 
-  accepts_nested_attributes_for :ingredients,
-                                reject_if: proc {|attributes| attributes['name'].blank?},
-                                allow_destroy: true
-  accepts_nested_attributes_for :directions,
-                                reject_if: proc {|attributes| attributes['step'].blank?},
-                                allow_destroy: true
+  	has_many :ingredients, :inverse_of => :recipe
+  	has_many :directions, :inverse_of => :recipe
 
-  validates :title, :description, :image, presence: true
+  	accepts_nested_attributes_for :ingredients,
+    															reject_if: proc { |attributes| attributes['name'].blank? },
+    															allow_destroy: true
+   	accepts_nested_attributes_for :directions,
+    															reject_if: proc { |attributes| attributes['step'].blank? },
+    															allow_destroy: true
 
-  has_attached_file :image, styles: { medium: "400x400#" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    validates :title, :description, :image, presence: true
+
+  	has_attached_file :image, styles: { :medium => "400x400#"}
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
